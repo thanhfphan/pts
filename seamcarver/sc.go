0d6b2d632bc2938c11ec605d2f1dd4af93655e50
thanhfphan <thanhptse@gmail.com>
2024-10-14 02:36:16 +0700

    implement find vertical seam

--- seamcarver/sc.go
+++ seamcarver/sc.go
@@ -1,9 +1,12 @@
 package seamcarver
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"math"
+	"strconv"
+	"strings"
 )
 
 type SeamCarver struct {
@@ -23,35 +26,6 @@ func New(img image.Image) *SeamCarver {
 	return sc
 }
 
-func imageToArrayPixel(img image.Image) [][]*Pixel {
-	w, h := img.Bounds().Dx(), img.Bounds().Dy()
-	arr := make([][]*Pixel, h)
-	for y := 0; y < h; y++ {
-		arr[y] = make([]*Pixel, w)
-		for x := 0; x < w; x++ {
-			arr[y][x] = &Pixel{
-				C: img.At(x, y),
-			}
-		}
-	}
-	return arr
-}
-
-func arrayPixelToImage(arr [][]*Pixel) image.Image {
-	h := len(arr)
-	w := len(arr[0])
-
-	img := image.NewRGBA(image.Rect(0, 0, w, h))
-
-	for y := 0; y < h; y++ {
-		for x := 0; x < w; x++ {
-			img.Set(x, y, arr[y][x].C)
-		}
-	}
-
-	return img
-}
-
 func (sc *SeamCarver) RecalculateEnergy() {
 	h := len(sc.pixels)
 	w := len(sc.pixels[0])
@@ -64,21 +38,8 @@ func (sc *SeamCarver) RecalculateEnergy() {
 				continue
 			}
 
-			rplus, gplus, bplus, _ := sc.pixels[y][x+1].C.RGBA()
-			rminus, gminus, bminus, _ := sc.pixels[y][x-1].C.RGBA()
-			rplus, gplus, bplus = rplus/257, gplus/257, bplus/257
-			rminus, gminus, bminus = rminus/257, gminus/257, bminus/257
-
-			deltaX := (rplus-rminus)*(rplus-rminus) + (gplus-gminus)*(gplus-gminus) + (bplus-bminus)*(bplus-bminus)
-
-			// note result variable
-			rplus, gplus, bplus, _ = sc.pixels[y+1][x].C.RGBA()
-			rplus, gplus, bplus = rplus/257, gplus/257, bplus/257
-
-			rminus, gminus, bminus, _ = sc.pixels[y-1][x].C.RGBA()
-			rminus, gminus, bminus = rminus/257, gminus/257, bminus/257
-
-			deltaY := (rplus-rminus)*(rplus-rminus) + (gplus-gminus)*(gplus-gminus) + (bplus-bminus)*(bplus-bminus)
+			deltaX := calcDeltaSquare(sc.pixels[y][x+1].C, sc.pixels[y][x-1].C)
+			deltaY := calcDeltaSquare(sc.pixels[y+1][x].C, sc.pixels[y-1][x].C)
 
 			sc.pixels[y][x].E = math.Sqrt(float64(deltaX) + float64(deltaY))
 		}
@@ -111,12 +72,16 @@ func (sc *SeamCarver) Color(x, y int) color.Color {
 
 // FindHorizontalSeam return sequence of indices for horizontal seam
 func (sc *SeamCarver) FindHorizontalSeam() []int {
+	// TODO: implement
+	// - rotate
+	// - find vertical seam
+	// - rotate back
 	panic("unimplement")
 }
 
 // FindVerticalSeam return sequence of indices for vertical seam
 func (sc *SeamCarver) FindVerticalSeam() []int {
-	panic("unimplement")
+	return sc.findShortestPath()
 }
 
 // RemoveHorizontalSeam from current picture
@@ -127,4 +92,118 @@ func (sc *SeamCarver) RemoveHorizontalSeam(seam []int) {
 // RemoveVerticalSeam from current picture
 func (sc *SeamCarver) RemoveVerticalSeam(seam []int) {
 	panic("unimplement")
+}
+
+func (sc *SeamCarver) findShortestPath() []int {
+	height, width := sc.Height(), sc.Width()
+	// adj return the neighbors of the pixel at(x, y) to pixels (x − 1, y + 1), (x, y + 1), and (x + 1, y + 1),
+	adj := func(y, x int) [][]int {
+		r := [][]int{}
+		if y+1 < height && x-1 >= 0 {
+			r = append(r, []int{y + 1, x - 1})
+		}
+		if y+1 < height {
+			r = append(r, []int{y + 1, x})
+		}
+		if y+1 < height && x+1 < width {
+			r = append(r, []int{y + 1, x + 1})
+		}
+
+		return r
+	}
+	// travel graph
+	distTo := map[string]float64{}
+	pathTo := map[string]string{}
+	var dfs func(int, int)
+	dfs = func(y, x int) {
+		k := encode(y, x)
+		for _, arr := range adj(y, x) {
+			visitkey := encode(arr[0], arr[1])
+			pixel := sc.pixels[arr[0]][arr[1]]
+			val, has := distTo[visitkey]
+			if !has || val > distTo[k]+pixel.E {
+				distTo[visitkey] = distTo[k] + pixel.E
+				pathTo[visitkey] = k
+			}
+			dfs(arr[0], arr[1])
+		}
+	}
+	for x := 0; x < width; x++ {
+		distTo[encode(0, x)] = 0.0
+		dfs(0, x)
+	}
+
+	// get shortest path
+	var smallest string
+	for x := 0; x < width; x++ {
+		k := encode(height-1, x)
+		if smallest == "" || distTo[k] < distTo[smallest] {
+			smallest = k
+		}
+	}
+	path := []int{}
+	current := smallest
+	for current != "" {
+		_, tmpx := decode(current)
+		path = append(path, tmpx)
+		current = pathTo[current]
+	}
+	// reverse
+	for i, j := 0, len(path)-1; i < j; i, j = i+1, j-1 {
+		path[i], path[j] = path[j], path[i]
+	}
+
+	return path
+}
+
+func encode(y, x int) string {
+	return fmt.Sprintf("%d-%d", y, x)
+}
+
+func decode(key string) (int, int) {
+	items := strings.Split(key, "-")
+	y, _ := strconv.Atoi(items[0])
+	x, _ := strconv.Atoi(items[1])
+	return y, x
+}
+
+func imageToArrayPixel(img image.Image) [][]*Pixel {
+	w, h := img.Bounds().Dx(), img.Bounds().Dy()
+	arr := make([][]*Pixel, h)
+	for y := 0; y < h; y++ {
+		arr[y] = make([]*Pixel, w)
+		for x := 0; x < w; x++ {
+			arr[y][x] = &Pixel{
+				C: img.At(x, y),
+			}
+		}
+	}
+	return arr
+}
+
+func arrayPixelToImage(arr [][]*Pixel) image.Image {
+	h := len(arr)
+	w := len(arr[0])
+
+	img := image.NewRGBA(image.Rect(0, 0, w, h))
+
+	for y := 0; y < h; y++ {
+		for x := 0; x < w; x++ {
+			img.Set(x, y, arr[y][x].C)
+		}
+	}
+
+	return img
+}
+
+func calcDeltaSquare(c1, c2 color.Color) uint32 {
+	r, g, b, _ := c1.RGBA()
+	r, g, b = r/257, g/257, b/257
+
+	r2, g2, b2, _ := c2.RGBA()
+	r2, g2, b2 = r2/257, g2/257, b2/257
+
+	d := (r-r2)*(r-r2) + (g-g2)*(g-g2) + (b-b2)*(b-b2)
+
+	return d
 }
---
+++ tools/print_seam/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"image"
+	_ "image/gif"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"path/filepath"
+
+	"thanhfphan.com/pts/seamcarver"
+)
+
+var path = flag.String("image", "demo.png", "the path to the image")
+
+func main() {
+	flag.Parse()
+
+	picture, err := openImage(*path)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s (%d-by-%d image)\n", *path, picture.Bounds().Dx(), picture.Bounds().Dy())
+	fmt.Println()
+
+	carver := seamcarver.New(picture)
+	verticalSeam := carver.FindVerticalSeam()
+	fmt.Println("Vertical seam:", verticalSeam)
+	// fmt.Println("Horizontal seam:", carver.FindHorizontalSeam())
+	fmt.Println()
+	fmt.Println("Printing energy calculated for each pixel.")
+	for row := 0; row < carver.Height(); row++ {
+		for col := 0; col < carver.Width(); col++ {
+			var c string
+			if verticalSeam[row] == col {
+				c = "*" // marked
+			}
+			fmt.Printf("%10.2f%s", carver.Energy(col, row), c)
+		}
+		fmt.Println()
+	}
+}
+
+func openImage(path string) (image.Image, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	ext := filepath.Ext(path)
+	switch ext {
+	case ".png":
+		img, err := png.Decode(file)
+		if err != nil {
+			return nil, fmt.Errorf("png.Decode '%s' error: %w", path, err)
+		}
+
+		return img, nil
+	case ".jpg", ".jpeg":
+		img, err := jpeg.Decode(file)
+		if err != nil {
+			return nil, fmt.Errorf("jpeg.Decode '%s' error: %w", path, err)
+		}
+
+		return img, nil
+	default:
+		// Try to decode any format supported by image.Decode
+		img, _, err := image.Decode(file)
+		if err != nil {
+			return nil, fmt.Errorf("try to decode image '%s' error: %w", path, err)
+		}
+
+		return img, nil
+	}
+
+}
